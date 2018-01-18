e55641ba292605cc3bf662bc591e9d49cdcad1af
wyh <playmusichappy@126.com>
2018-01-18 11:44:15 +0800

    样式修改

---
+++ jdshop/src/components/news.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="scroll-wrap">
+		<!--滚动新闻页面-->
+		<ul class="scroll-content" :style="{ top }">
+			<li v-for="item in prizeList">{{item.name}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				prizeList: [{
+						name: 0
+					},
+					{
+						name: 1
+					},
+					{
+						name: 2
+					},
+					{
+						name: 3
+					},
+					{
+						name: 4
+					}
+				],
+				activeIndex: 0
+			}
+		},
+		methods: {
+
+		},
+		mounted() {
+			setInterval(_ => {
+				if(this.activeIndex < this.prizeList.length) {
+					this.activeIndex += 1;
+				} else {
+					this.activeIndex = 0;
+				}
+			}, 1000);
+		}
+	}
+</script>
+
+<style>
+	.scroll-wrap {
+		width: 200px;
+		height: 50px;
+		border: 1px solid blue;
+		overflow: hidden;
+	}
+	
+	.scroll-content {
+		position: relative;
+		transition: top 0.5s;
+		li {
+			line-height: 50px;
+			text-align: center;
+		}
+	}
+</style>
--- jdshop/src/page/home/home.vue
+++ jdshop/src/page/home/home.vue
@@ -1,48 +1,54 @@
 <template>
 	<div class="demo-infinite-container">
-		<swiper :options="swiperOption">
-			<swiper-slide v-for="(item,index) in img" :key="index">
-				<img :src="item.url" class="swimg" />
-			</swiper-slide>
+		<div class="clearfix swp">
+			<swiper :options="swiperOption" class="clearfix">
+				<swiper-slide v-for="(item,index) in img" :key="index" class="clearfix">
+					<img :src="item.url" class="swimg " />
+				</swiper-slide>
 
-			<div class="swiper-pagination" slot="pagination"></div>
-			<div class="swiper-button-prev" slot="button-prev"></div>
-			<div class="swiper-button-next" slot="button-next"></div>
-		</swiper>
-		<nav class="center-nav">
-			<ul class="center-nav-items">
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec8c2N5d23879c.png" alt="京东超市" /><span>京东超市</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ee7abNef54e799.png" alt="全球购" /><span>全球购</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec95dN10b18c94.png" alt="京东服饰" /><span>京东服饰</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec985N8d10b0c5.png" alt="京东生鲜" /><span>京东生鲜</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec9a6Ne0f3804e.png" alt="排行榜" /><span>排行榜</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec9bbN5660203b.png" alt="充值缴费" /><span>充值缴费</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec9d7Ncbf13902.png" alt="领金豆" /><span>领金豆</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5ec9f2N4ab56374.png" alt="领券" /><span>领券</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5eebfeNe33ef422.png" alt="赚钱" /><span>赚钱</span></a>
-				</li>
-				<li class="item-li">
-					<a href="#"><img src="../../assets/5a5eca37N74154d4f.png" alt="物流查询" /><span>物流查询</span></a>
-				</li>
-			</ul>
-		</nav>
+				<div class="swiper-pagination" slot="pagination"></div>
+				<div class="swiper-button-prev" slot="button-prev"></div>
+				<div class="swiper-button-next" slot="button-next"></div>
+			</swiper>
+		</div>
+		<div class="center-nav-outer clearfix">
+			<nav class="center-nav clearfix">
+				<ul class="center-nav-items clearfix">
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec8c2N5d23879c.png" alt="京东超市" /><span>京东超市</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ee7abNef54e799.png" alt="全球购" /><span>全球购</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec95dN10b18c94.png" alt="京东服饰" /><span>京东服饰</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec985N8d10b0c5.png" alt="京东生鲜" /><span>京东生鲜</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec9a6Ne0f3804e.png" alt="排行榜" /><span>排行榜</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec9bbN5660203b.png" alt="充值缴费" /><span>充值缴费</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec9d7Ncbf13902.png" alt="领金豆" /><span>领金豆</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5ec9f2N4ab56374.png" alt="领券" /><span>领券</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5eebfeNe33ef422.png" alt="赚钱" /><span>赚钱</span></a>
+					</li>
+					<li class="item-li">
+						<a href="#"><img src="../../assets/5a5eca37N74154d4f.png" alt="物流查询" /><span>物流查询</span></a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+		<news class="scroll-news clearfix"></news>
 		<!--<mu-list>
 			<template v-for="item in list">
 				<mu-list-item :title="item" />
@@ -55,6 +61,7 @@
 
 <script>
 	import { mapState } from 'vuex';
+	import News from 'components/news'
 	export default {
 		data() {
 			const list = []
@@ -121,6 +128,9 @@
 		computed: {
 			//...mapState(["topimgs"]),
 		},
+		components: {
+			News
+		},
 	}
 </script>
 
@@ -131,44 +141,65 @@
 		overflow: auto;
 		-webkit-overflow-scrolling: touch;
 		border: 1px solid #d9d9d9;
-		.swimg {
+		position: relative;
+		.swp {
+			/*position: absolute;*/
 			width: 100%;
-			height: 360/@bs
-		}
-		.center-nav {
-			position: absolute;
-			width: 100%;
-			background: url(../../assets/nav-bgimg.jpg) no-repeat fixed;
-			background-size: 750/@bs 100%;
-			.center-nav-items {
+			.swimg {
 				width: 100%;
-				box-sizing: border-box;
-				list-style: none;
-				li {
-					float: left;
-					width: 20%;
+				height: 360/@bs;
+			}
+			/*.swp::before,
+			.swp::after {
+				content: "";
+				display: block;
+				visibility: hidden;
+				height: 0;
+				line-height: 0;
+				clear: both;
+			}*/
+		}
+		.center-nav-outer {
+			width: 100%;
+			.center-nav {
+				/*position: absolute;*/
+				width: 100%;
+				background: url(../../assets/nav-bgimg.jpg) no-repeat fixed;
+				background-size: 750/@bs 100%;
+				.center-nav-items {
+					width: 100%;
 					box-sizing: border-box;
-					padding: 5px;
-					a {
-						width: 100%;
-						display: block;
-						img {
-							display: block;
-							width: 90/@bs;
-							height: 90/@bs;
-							margin: 0 auto;
-						}
-						span {
-							font-size: 12/@bs;
-							color: #fff;
-							padding-top: 3/@bs;
-							text-align: center;
-							display: block;
+					list-style: none;
+					li {
+						float: left;
+						width: 20%;
+						box-sizing: border-box;
+						padding: 5px;
+						a {
 							width: 100%;
+							display: block;
+							img {
+								display: block;
+								width: 90/@bs;
+								height: 90/@bs;
+								margin: 0 auto;
+							}
+							span {
+								font-size: 12/@bs;
+								color: #fff;
+								padding-top: 3/@bs;
+								text-align: center;
+								display: block;
+								width: 100%;
+							}
 						}
 					}
 				}
 			}
+		}
+		.scroll-news {
+			/*position: absolute;*/
+			width: 100%;
 		}
 	}
 </style>
