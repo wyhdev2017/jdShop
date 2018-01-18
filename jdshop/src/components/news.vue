1ea3cdfb06c106f85c93cf0dcf8302646ff28803
wyh <playmusichappy@126.com>
2018-01-18 13:09:44 +0800

    备份

--- jdshop/src/components/news.vue
+++ jdshop/src/components/news.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="scroll-wrap">
-		<!--滚动新闻页面-->
-		<ul class="scroll-content" :style="{ top }">
-			<li v-for="item in prizeList">{{item.name}}</li>
+	<!--滚动新闻窗口-->
+	<div class="box">
+		<ul id="con1" ref="con1" :class="{anim:animate==true}">
+			<li v-for='item in items'>{{item.name}}</li>
 		</ul>
 	</div>
 </template>
@@ -11,54 +11,47 @@
 	export default {
 		data() {
 			return {
-				prizeList: [{
-						name: 0
+				animate: false,
+				items: [{
+						name: "马云"
 					},
 					{
-						name: 1
+						name: "雷军"
 					},
 					{
-						name: 2
-					},
-					{
-						name: 3
-					},
-					{
-						name: 4
+						name: "王勤"
 					}
-				],
-				activeIndex: 0
+				]
 			}
 		},
-		methods: {
-
+		created() {
+			setInterval(this.scroll, 1500)
 		},
-		mounted() {
-			setInterval(_ => {
-				if(this.activeIndex < this.prizeList.length) {
-					this.activeIndex += 1;
-				} else {
-					this.activeIndex = 0;
-				}
-			}, 1000);
+		methods: {
+			scroll() {
+				this.animate = true; // 因为在消息向上滚动的时候需要添加css3过渡动画，所以这里需要设置true
+				setTimeout(() => { //  这里直接使用了es6的箭头函数，省去了处理this指向偏移问题，代码也比之前简化了很多
+					this.items.push(this.items[0]); // 将数组的第一个元素添加到数组的
+					this.items.shift(); //删除数组的第一个元素
+					this.animate = false; // margin-top 为0 的时候取消过渡动画，实现无缝滚动
+				}, 500)
+			}
 		}
 	}
 </script>
 
-<style>
-	.scroll-wrap {
-		width: 200px;
-		height: 50px;
-		border: 1px solid blue;
-		overflow: hidden;
+<style scoped="scoped" lang="less">
+	@import url("../common/less/variables.less");
+	.anim {
+		transition: all 0.5s;
+		margin-top: -30px;
 	}
-	
-	.scroll-content {
-		position: relative;
-		transition: top 0.5s;
-		li {
-			line-height: 50px;
-			text-align: center;
-		}
+	ul{
+		list-style: none;
+	}
+	#con1 li {
+		list-style: none;
+		line-height: 30px;
+		height: 30px;
 	}
 </style>
